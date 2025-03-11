77251fe1e350e0db18c8eeffc8cc9273a9758ebf
Chatpong Deepet <chatpond@outlook.com>
2025-03-11 13:31:43 +0700

    [Update] input form state

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -5,7 +5,7 @@ const AddPage = () => {
 		<section className = "bg-blue-50">
 			<div className = "container m-auto max-w-2xl py-24">
 				<div
-					className = "bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
+					className = "bg-white px-6 py-8 mb-4 shadow-md rounded-md border border-gray-400 m-4 md:m-0"
 				>
 					<PropertyAddForm />
 				</div>
